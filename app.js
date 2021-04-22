8011a9b0e73eb6f33499d4212bd4003652406a8d
ledanhhung <hungldph11449@fpt.edu.vn>
2021-04-22 22:15:06 +0700

    ádasd

--- app.js
+++ app.js
@@ -60,9 +60,95 @@ db.once("open", function () {
   console.log("kết nối thành công");
 
 });
+function statusCode(code = undefined, message = undefined, data = undefined) {
+  return (baseJson = {
+    code: code,
+    message: message,
+    data: data,
+  });
+}
+// sua 
+// app.put('/suaUser', function (req, res,next) {
+//   upload(req, res, function (err) {
+//     if (err instanceof multer.MulterError) {
+//       res.send(err);
+//     } else if (err) {
+//       res.json(statusCode(400, err));
+//     } else {
+//       var update = {
+//         name: req.body.name,
+//         password: req.body.password,
+//         date: req.body.date,
+//         email: req.body.email,
+//         gioitinh: req.body.gioitinh,
+//         sothich: req.body.sothich,
+//         gioithieu: req.body.gioithieu,
+//        // avatars: req.files.map((file) => "upload/" + file.filename),
+//       };
+//       connectUsers
+//         .findByIdAndUpdate(mongoose.Types.ObjectId(req.body.id), update)
+//         .then(function (user) {
+//           if (user) {
+//             // thành công
+//             res.json(statusCode(200, "Success", user));
+//           } else {
+//             // không tìm thấy user theo id nhận đc
+//             res.json(statusCode(404, "Not Found", err));
+//           }
+//         })
+//         .catch((err) => {
+//           // lỗi server khong thể update
+//           res.json(statusCode(500, "Server Error", err));
+//         });
+//     }
+//   });
+// });
+// óa react
+app.delete('/delete2', function (req, res, next) {
 
+  connectUsers.deleteOne({ _id: req.body._id }, function (err, user) {
+    return err
+      ? res.json(statusCode(404, "Not Found", err))
+      : res.json(statusCode(200, "Success", user));
+  });
+
+})
+//them react
+app.post('/insertUser2', function (req, res) {
+  console.log('hbjnkml,')
+  upload(req, res, function (err) {
+
+    if (err instanceof multer.MulterError) {
+      res.send(err);
+    } else if (err) {
+      res.json(statusCode(400, err));
+    } else {
+      connectUsers = db.model("users", user);
+
+      // userSchema thay bằng schema giống ở hàm update làm ở lab67
+      const user1 = new connectUsers({
+        // userName, password,… viết theo đối tượng trên csdl
+        name: req.body.name,
+        password: req.body.password,
+        date: req.body.date,
+        email: req.body.email,
+        gioitinh: req.body.gioitinh,
+        sothich: req.body.sothich,
+        gioithieu: req.body.gioithieu,
+        //  avatars: req.files.map((file) => "upload/" + file.filename),
+      });
+      user1.save(function (err) {
+        // lỗi không thể thêm
+        if (err) res.json(statusCode(500, "Server Error", err));
+        // thêm thành công
+        else res.json(statusCode(200, "Success", user1));
+      });
+    }
+  })
+});
+// them sever
 app.post("/insertUser", (req, res, next) => {
-  upload(req,res,function(err){
+  upload(req, res, function (err) {
     if (err instanceof multer.MulterError) {
       // A Multer error occurred when uploading.
       console.log(err);
@@ -70,7 +156,7 @@ app.post("/insertUser", (req, res, next) => {
       // An unknown error occurred when uploading.
       console.log(err);
     }
-    else{
+    else {
       connectUsers = db.model("users", user);
       connectUsers({
         email: req.body.email,
@@ -90,15 +176,16 @@ app.post("/insertUser", (req, res, next) => {
       });
     }
   })
- 
+
+
 });
 
 app.get('/login', function (request, response) {
 
   response.render('login');
 });
-app.get('/edit/:id',function(request,response){
-  response.render('edit',{id:request.params.id});
+app.get('/edit/:id', function (request, response) {
+  response.render('edit', { id: request.params.id });
 })
 app.get('/dangki', function (request, response) {
   response.render('dangki');
@@ -125,81 +212,92 @@ app.get('/userApi', function (request, response) {
   });
 });
 // xoa
-app.post('/:id',function (request,respone) {
-  connectUsers = db.model("users", user);
-  connectUsers.deleteOne({ _id: request.params.id},function(err,user){
-    console.log(request.params.id);
-    respone.redirect("/user");
+app.post('/deleteUser', function (request, respone) {
+  console.log(request.body)
+  var connectUsers = db.model("users", user);
+  connectUsers.findByIdAndDelete( request.body._id, function (err) {
+    // if(err){
+    //   console.log(err)
+    // }
+    // else{
+    //   respone.json()
+    // }
+    // console.log(request.params.id);
+    // respone.redirect("/user");
+
+    if (err) {
+      respone.json({ errCode: 500, errMess: err })
+    }
+    else {
+      connectUsers.find({}).then(users => {
+        respone.render('user', { users: users.map(obj => obj.toJSON(obj)) })
+        respone.json({ errCode: "Xóa thành công!", errMess: 200 })
+      })
+    }
   })
 });
 // sua 
-app.post("/updateUser/:id", (req, res, next) => {
-  connectUsers = db.model("users", user);
-  upload(req, res, function (err) {
-    if (err instanceof multer.MulterError) {
-      res.send(err);
-    } else if (err) {
-      res.send(err);
-    } else {
-      var update = {
-        email: req.body.email,
-        password: req.body.password,
-        name: req.body.name,
-        date: req.body.date,
-        gioitinh: req.body.gioitinh,
-        sothich: req.body.sothich,
-        gioithieu: req.body.gioithieu,
-        avatars: req.body.avatars,
-       
-      };
-      connectUsers
-        .findByIdAndUpdate(mongoose.Types.ObjectId(req.params.id), update)
-        .then(function (user) {
-          res.redirect("/user");
-        })
-        .catch((err) => console.log(err));
-    }
-  });
+// app.post("/updateUser", (req, res, next) => {
 
-});
-
-// function statusCode(code = undefined, message = undefined, data = undefined) {
-//   return (baseJson = {
-//     code: code,
-//     message: message,
-//     data: data,
-//   });
-//}
-//them user 
-// router.get("/themUser", function (req, res, next) {
 //   upload(req, res, function (err) {
 //     if (err instanceof multer.MulterError) {
 //       res.send(err);
 //     } else if (err) {
-//       res.json(statusCode(400, err));
+//       res.send(err);
 //     } else {
-//       // userSchema thay bằng schema giống ở hàm update làm ở lab67
-//       const user = new Schema({
-//         // userName, password,… viết theo đối tượng trên csdl
-//         name: req.body.name,
-//         password: req.body.password,
-//         date: req.body.date,
+//       var update = {
 //         email: req.body.email,
+//         password: req.body.password,
+//         name: req.body.name,
+//         date: req.body.date,
 //         gioitinh: req.body.gioitinh,
 //         sothich: req.body.sothich,
 //         gioithieu: req.body.gioithieu,
-//       //  avatars: req.files.map((file) => "upload/" + file.filename),
-//       });
-//       user.save(function (err) {
-//         // lỗi không thể thêm
-//         if (err) res.json(statusCode(500, "Server Error", err));
-//         // thêm thành công
-//         else res.json(statusCode(200, "Success", user));
-//       });
-//     }
-//   });
+//         avatars: req.body.avatars,
+
+//       };
+//       connectUsers = db.model("users", user).updateOne({ _id: req.body._id }, update, function (err, user) {
+//           if(err){
+//             res.json(statusCode(500,"Error",null))
+//           }
+//           else{
+//             // res.redirect('/user')
+//             res.json(statusCode(200, "Ok", user))
+//           }
+//       })
+
+//       // connectUsers
+//       //   .findByIdAndUpdate(mongoose.Types.ObjectId(req.params.id), update)
+//       //   .then(function (user) {
+//       //     res.redirect("/user");
+//       //   })
+//       //   .catch((err) => console.log(err));
+
+//   }});
 
 // });
+
+app.post('/updateUser', function (req, res) {
+  var update = {
+    email: req.body.email,
+    password: req.body.password,
+    name: req.body.name,
+    date: req.body.date,
+    gioitinh: req.body.gioitinh,
+    sothich: req.body.sothich,
+    gioithieu: req.body.gioithieu,
+    avatars: req.body.avatars,
+  };
+  connectUsers = db.model("users", user).updateOne({ _id: req.body._id }, update, function (err, user) {
+    if (err) {
+      res.json({ errCode: 500, errMess: err })
+    }
+    else {
+      res.render('user')
+      res.json({ errCode: 200, errMess: "Ok" })
+    }
+  })
+})
 
 
 
@@ -214,16 +312,16 @@ app.post('/dangki', function (request, response) {
 app.post('/insertUser', function (request, response) {
   response.redirect('/user');
 });
-app.post('/user',function(request,response) {
+app.post('/user', function (request, response) {
   response.redirect('/user')
 });
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
